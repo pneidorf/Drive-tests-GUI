1a29fff04514308793ccbb3ef2530880808c9ba1
dmbabenko9 <dmbabenko9@gmail.com>
2024-05-20 17:44:57 +0700

    rename Android folder

--- frontend/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as ReactDOMClient from 'react-dom/client'
-import App from './App';
-import './css/index.css';
-
-const app = ReactDOMClient.createRoot(document.getElementById("app"))
-
-app.render(<App />)
